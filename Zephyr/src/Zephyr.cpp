c681d4666a4edbf2a4ca1571a8bafa21b8d38718
los <losiu99@gazeta.pl>
2013-06-27 18:52:51 +0200

    Basic setup (libraries, example program)

--- Zephyr/src/Zephyr.cpp
+++ Zephyr/src/Zephyr.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-    std::cout << "Zephyr" << std::endl;
+#include <GL/glew.h>
+#include <GL/glfw.h>
+#include <glm/glm.hpp>
+
+using namespace glm;
+
+int main(int argc, char* argv[])
+{
     return 0;
 }
