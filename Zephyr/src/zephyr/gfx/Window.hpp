3e3d92f4e5d28dcc4cbacc6514ab532b38914066
los <losiu99@gazeta.pl>
2013-08-29 19:59:44 +0200

    Moved developement to Eclipse. Added Eclipse configuration.

--- Zephyr/src/zephyr/gfx/Window.hpp
+++ Zephyr/src/zephyr/gfx/Window.hpp
@@ -15,14 +15,24 @@ namespace zephyr
 {
 namespace gfx
 {
-    
+
+/**
+ * System-level window, providing basic graphics output functionality.
+ */
 class Window
 {
 public:
+    /**
+     * Creates @ref Window with specified size and title displayed on the
+     * titlebar.
+     */
     Window(int width, int height, const std::string& title);
     
     void pollEvents();
     
+    /**
+     * Swaps graphic buffers of the window.
+     */
     void swapBuffers() const;
     
 private:
