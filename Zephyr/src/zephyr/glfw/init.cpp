1e9c0fb7aa002b641f27195f3c044cc30da712fb
los <losiu99@gazeta.pl>
2013-08-29 20:23:08 +0200

    Comments for GLFW initializer. Moving to different bracing convention, time to check if it works for C++.

--- Zephyr/src/zephyr/glfw/init.cpp
+++ Zephyr/src/zephyr/glfw/init.cpp
@@ -1,3 +1,8 @@
+/**
+ * @file init.cpp
+ *
+ * Initialization code for GLFW.
+ */
 
 #include <iostream>
 #define GLEW_STATIC
@@ -5,37 +10,38 @@
 #include <GLFW/glfw3.h>
 
 // anonymous namespace
-namespace
-{
+namespace {
 
-void errorCallback(int code, const char* description)
-{
-    std::cerr << "[Error " << code << "]: " << description << std::endl;
+/**
+ * Error callback for GLFW framework.
+ */
+void errorCallback(int code, const char* description) {
+	std::cerr << "[Error " << code << "]: " << description << std::endl;
 }
-    
-struct InitGLFW
-{
-    InitGLFW() 
-    { 
-        glfwSetErrorCallback(errorCallback);
-        if (!glfwInit())
-        {
-            std::cerr << "GLFW error!" << std::endl;
-            std::exit(1);
-        }
-        else 
-        {
-            std::cout << "GLFW initialized" << std::endl;
-        }
-    }
-    
-    ~InitGLFW() 
-    { 
-        std::cout <<  "Shutting down GLFW" << std::endl;
-        glfwTerminate();
-        std::cout <<  "GLFW terminated" << std::endl;
-    }
-} 
+
+/**
+ * Plain old static object initializer. Constructor contains initialization
+ * code, destructor handles the cleanup. As long as the order of various
+ * components does not matter, it should be enough.
+ */
+struct InitGLFW {
+
+	InitGLFW() {
+		glfwSetErrorCallback(errorCallback);
+		if (!glfwInit()) {
+			std::cerr << "GLFW error!" << std::endl;
+			std::exit(1);
+		} else {
+			std::cout << "GLFW initialized" << std::endl;
+		}
+	}
+
+	~InitGLFW() {
+		std::cout << "Shutting down GLFW" << std::endl;
+		glfwTerminate();
+		std::cout << "GLFW terminated" << std::endl;
+	}
+}
 // static global object
 init;
 
