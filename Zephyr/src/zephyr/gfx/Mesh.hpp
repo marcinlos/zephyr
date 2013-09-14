2a3247fc8e4538f3a401265765fc12bb9afd62ef
los <losiu99@gazeta.pl>
2013-09-14 19:36:28 +0200

    Changed normal averaging, so that it's weighed by the triangle size

--- Zephyr/src/zephyr/gfx/Mesh.hpp
+++ Zephyr/src/zephyr/gfx/Mesh.hpp
@@ -84,14 +84,16 @@ std::vector<glm::vec3> generateNormals(const std::vector<glm::vec4>& vertices,
         };
         glm::vec3 ab { vertices[idx[1]] - vertices[idx[0]] };
         glm::vec3 ac { vertices[idx[2]] - vertices[idx[0]] };
-        glm::vec3 n = glm::normalize(glm::cross(ab, ac));
+        glm::vec3 n = glm::cross(ab, ac);
 
         for (int j = 0; j < 3; ++ j) {
             int k = idx[j];
             float c = 1.0f / ++ times[k];
-            normals[k] = glm::normalize(glm::mix(normals[k], n, c));
-
+            normals[k] += n;
         }
+    }
+    for (auto& n : normals) {
+        n = glm::normalize(n);
     }
     return normals;
 }
@@ -167,7 +169,7 @@ MeshData loadMeshData(const char* filename) {
     }
 
     data.normals = generateNormals(data.vertices, data.indices);
-    data.colors = randomColors(data.vertices.size());
+    data.colors = std::vector<glm::vec4>(data.vertices.size(), glm::vec4 { 1, 0, 0, 0});//randomColors(data.vertices.size());
     return data;
 }
 
